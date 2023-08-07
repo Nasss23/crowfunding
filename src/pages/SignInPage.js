3698340b685bb2ad7e477ed834bf582fd2ff09ff
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2023-08-07 20:03:04 +0700

    2 - CodeSignUp

---
+++ src/pages/SignInPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutAuthentication from '../layout/LayoutAuthentication';
+
+const SignInPage = () => {
+    return (
+        <LayoutAuthentication heading="Sign In">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae dolorum accusamus quidem repellendus quia cum delectus facilis commodi architecto facere voluptatem necessitatibus dolor a expedita aut nesciunt reprehenderit, id tempora?
+        </LayoutAuthentication>
+    );
+};
+
+export default SignInPage;
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutAuthentication from '../layout/LayoutAuthentication';
+
+const SignUpPage = () => {
+    return (
+        <LayoutAuthentication heading="Sign Up">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae dolorum accusamus quidem repellendus quia cum delectus facilis commodi architecto facere voluptatem necessitatibus dolor a expedita aut nesciunt reprehenderit, id tempora?
+        </LayoutAuthentication>
+    );
+};
+
+export default SignUpPage;
