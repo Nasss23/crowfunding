4c63e9176c602fb677d192d738a3fced948ac68d
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2023-08-07 14:00:49 +0700

    1 - SetUp

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,27 @@ module.exports = {
       fontFamily: {
         body: ["Epilogue", "sans-serif;"],
       },
+      colors: {
+        primary: "#1DC071",
+        secondary: "#6F49FD",
+        text1: "#3A3A43",
+        text2: "#4B5264",
+        text3: "#808191",
+        text4: "#B2B3BD",
+        "icon-color": "#808191",
+        white: "#FFFFFF",
+        whiteSoft: "#FCFBFF",
+        graySoft: "#FCFCFC",
+        strockSoft: "#F1F1F3",
+        lite: "#FCFCFD",
+        erroe: "#EB5757",
+        darkbg: "#EB5757",
+        darkSecondary: "#1C1C24",
+        softDark: "#22222C",
+        darkSoft: "#24242C",
+        darkStroke: "#3A3A43",
+        darkRed: "#422C32"
+      }
     },
   },
   plugins: [],
