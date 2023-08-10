aa81ce68e1eba20e6d66ace4a406ab2553f3f33a
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2023-08-10 12:28:40 +0700

    14 - ComponentSearch

---
+++ src/components/common/Overlay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Overlay = () => {
+    return (
+        <div>
+            <div className="fixed inset-0 z-40 invisible bg-black opacity-0 verlay bg-opacity-10"></div>
+
+        </div>
+    );
+};
+
+export default Overlay;
